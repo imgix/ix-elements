bdb280cb04c491a92a8a603b87e87a893024912a
Luis Ball <lball@imgix.com>
2023-01-09 13:08:37 -0800

    build: update svelte example to use ix-video

--- examples/svelte-kit/src/routes/+page.svelte
+++ examples/svelte-kit/src/routes/+page.svelte
@@ -3,13 +3,12 @@
 </script>
 
 <svelte:head>
-	<title>Browse Mux Elements</title>
+	<title>Browse ix-elements</title>
 </svelte:head>
 
 <nav>
 	<ul>
 		<li><a data-sveltekit-prefetch href="./mux-video" class="video">&lt;mux-video&gt;</a></li>
-		<li><a data-sveltekit-prefetch href="./mux-audio" class="audio">&lt;mux-audio&gt;</a></li>
 		<li><a data-sveltekit-prefetch href="./ix-video" class="player">&lt;ix-video&gt;</a></li>
 	</ul>
 </nav>
