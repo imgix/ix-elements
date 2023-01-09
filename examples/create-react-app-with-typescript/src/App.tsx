bcf3b0a58588bbca38dec2fe6f7305943778e47a
Luis Ball <lball@imgix.com>
2023-01-09 13:41:20 -0800

    build: update react example to use ix-video

--- examples/create-react-app-with-typescript/src/App.tsx
+++ examples/create-react-app-with-typescript/src/App.tsx
@@ -1,25 +1,24 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import MuxVideoPage from "./pages/MuxVideo";
-import MuxAudioPage from "./pages/MuxAudio";
 import IxVideoPage from "./pages/IxVideo";
 import IxVideoLazyPage from "./pages/IxVideoLazy";
-import MuxUploaderPage from "./pages/MuxUploader";
 
 function App() {
   return (
     <>
       <header>
         <div className="left-header">
-          <a className="mux-logo" href="https://www.mux.com/player" target="_blank" rel="noreferrer">
-            <img width="81" height="26" src="./images/mux-logo@2x.webp" alt="Mux logo" decoding="async" />
+          <a className="imgix-logo" href="https://www.imgix.com/video" target="_blank">
+            <img width="81" height="26" src="https://assets.imgix.net/imgix-logo.png?fm=auto&w=281&h=226" alt="imgix logo" decoding="async" />
           </a>
-          <h1><a href="/">Elements</a></h1>
+          <h1><a href="/">ix-elements</a></h1>
         </div>
         <div className="right-header">
-          <a className="github-logo" href="https://github.com/muxinc/elements" target="_blank" rel="noreferrer">
-            <img width="32" height="32" src="./images/github-logo.svg" alt="Github logo" />
+          <a className="github-logo" href="https://github.com/imgix/ix-elements" target="_blank">
+            <img width="32" height="32" src="https://avatars.githubusercontent.com/u/2793044?s=200&v=4" alt="Github logo" />
           </a>
         </div>
       </header>
@@ -27,10 +26,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="MuxVideo" element={<MuxVideoPage />} />
-          <Route path="MuxAudio" element={<MuxAudioPage />} />
           <Route path="IxVideo" element={<IxVideoPage />} />
           <Route path="IxVideoLazy" element={<IxVideoLazyPage />} />
-          <Route path="MuxUploader" element={<MuxUploaderPage />} />
         </Routes>
       </Router>
     </>
--- examples/create-react-app-with-typescript/src/pages/index.tsx
+++ examples/create-react-app-with-typescript/src/pages/index.tsx
@@ -5,9 +5,7 @@ function Home() {
     <nav>
       <ul>
         <li><Link to="./MuxVideo" className="video">&lt;MuxVideo&gt;</Link></li>
-        <li><Link to="./MuxAudio" className="audio">&lt;MuxAudio&gt;</Link></li>
         <li><Link to="./IxVideo" className="video">&lt;IxVideo&gt;</Link></li>
-        <li><Link to="./MuxUploader" className="uploader">&lt;MuxUploader&gt;</Link></li>
       </ul>
     </nav>
   );
