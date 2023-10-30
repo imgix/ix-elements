74ac64b9f4437353da9d797813bb6730f76d4a40
Luis Ball <luqven@gmail.com>
2023-10-30 11:49:29 -0700

    chore(cra-demo): update vidoe used example

--- examples/create-react-app-with-typescript/src/pages/IxPlayer.tsx
+++ examples/create-react-app-with-typescript/src/pages/IxPlayer.tsx
@@ -5,19 +5,22 @@ function IxPlayerPage() {
   return (
     <>
       <style>
-        {`ix-player {
+        {`
+        ix-player {
           display: block;
           width: 100%;
           aspect-ratio: 16 / 9;
           margin: 1rem 0 2rem;
+          --media-object-fit: cover;
+          --media-object-position: center;
         }`}
       </style>
 
       <IxPlayer
-        src="https://assets.imgix.video/videos/alexa_ski_big_bear_mountain.MOV?fm=hls"
+        src="https://assets.imgix.video/skeleton_halloween_candle.mp4"
+        posterParams="mark-x=1600&mark-y=25&mark-fit=crop&mark-w=300&mark64=aHR0cHM6Ly9hc3NldHMuaW1naXgubmV0L3ByZXNza2l0L2ltZ2l4LXByZXNza2l0LnBkZj93PTE2MCZmbT1wbmcmcGFnZT00"
+        title="Happy Halloween"
         type="hls"
-        gifPreview
-        posterParams="video-thumbnail-time=2&mark-align=top,right&mark64=aHR0cHM6Ly9hc3NldHMuaW1naXgubmV0L3ByZXNza2l0L2ltZ2l4LXByZXNza2l0LnBkZj93PTE2MCZmbT1wbmcmcGFnZT00"
         muted
       />
 
